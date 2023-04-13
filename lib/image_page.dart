aa53999f01ceb6ed02a9707f10b145c42e2ccfe7
bierbaumtim <tb2706@outlook.de>
2023-04-13 22:25:03 +0200

    add visibility detector

--- lib/image_page.dart
+++ lib/image_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class ImagePage extends StatelessWidget {
   const ImagePage({super.key});
@@ -33,15 +34,19 @@ class ImagePage extends StatelessWidget {
         future: _loadImagePage,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return LazyLoadScrollView(
-              child: ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) => _ImageCard(
-                  url: snapshot.data!.elementAt(index),
+            return VisibilityDetector(
+              key: const ValueKey('test'),
+              onVisibilityChanged: (info) {},
+              child: LazyLoadScrollView(
+                child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) => _ImageCard(
+                    url: snapshot.data!.elementAt(index),
+                  ),
                 ),
-              ),
-              onEndOfPage: () async => await Future.delayed(
-                const Duration(milliseconds: 1500),
+                onEndOfPage: () async => await Future.delayed(
+                  const Duration(milliseconds: 1500),
+                ),
               ),
             );
           }
