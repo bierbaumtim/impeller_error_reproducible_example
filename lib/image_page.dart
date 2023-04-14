4ce7e8665e5c0db56586d835afd45d1f1a874093
bierbaumtim <tb2706@outlook.de>
2023-04-14 10:22:59 +0200

    switch to cupertino app

--- lib/image_page.dart
+++ lib/image_page.dart
@@ -1,14 +1,18 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
-import 'package:visibility_detector/visibility_detector.dart';
 
 class ImagePage extends StatelessWidget {
-  const ImagePage({super.key});
+  const ImagePage({
+    super.key,
+    required this.onToggleShowPerformanceOverlay,
+  });
+
+  final VoidCallback onToggleShowPerformanceOverlay;
 
   Future<List<String>> get _loadImagePage async {
     final res = await http.get(
@@ -26,37 +30,81 @@ class ImagePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Impeller'),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Home',
+          ),
+        ],
       ),
-      body: FutureBuilder<List<String>>(
-        future: _loadImagePage,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return VisibilityDetector(
-              key: const ValueKey('test'),
-              onVisibilityChanged: (info) {},
-              child: LazyLoadScrollView(
-                child: ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) => _ImageCard(
-                    url: snapshot.data!.elementAt(index),
-                  ),
+      tabBuilder: (context, index) => CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: const Text('Impeller'),
+          trailing: CupertinoButton(
+            onPressed: onToggleShowPerformanceOverlay,
+            child: const Icon(CupertinoIcons.graph_square),
+          ),
+        ),
+        child: FutureBuilder<List<String>>(
+          future: _loadImagePage,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) => _ImageCard(
+                  url: snapshot.data!.elementAt(index),
                 ),
-                onEndOfPage: () async => await Future.delayed(
-                  const Duration(milliseconds: 1500),
-                ),
-              ),
-            );
-          }
+              );
+            }
 
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
       ),
     );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Impeller'),
+    //   ),
+    //   body: FutureBuilder<List<String>>(
+    //     future: _loadImagePage,
+    //     builder: (context, snapshot) {
+    //       if (snapshot.hasData) {
+    //         return VisibilityDetector(
+    //           key: const ValueKey('test'),
+    //           onVisibilityChanged: (info) {},
+    //           child: LazyLoadScrollView(
+    //             child: ListView.builder(
+    //               itemCount: snapshot.data!.length,
+    //               itemBuilder: (context, index) => _ImageCard(
+    //                 url: snapshot.data!.elementAt(index),
+    //               ),
+    //             ),
+    //             onEndOfPage: () async => await Future.delayed(
+    //               const Duration(milliseconds: 1500),
+    //             ),
+    //           ),
+    //         );
+    //       }
+
+    //       return const Center(
+    //         child: CircularProgressIndicator(),
+    //       );
+    //     },
+    //   ),
+    // );
   }
 }
 
@@ -71,9 +119,14 @@ class _ImageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Container(
       margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
       clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        color: CupertinoColors.secondarySystemGroupedBackground
+            .resolveFrom(context),
+        borderRadius: BorderRadius.circular(8),
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
@@ -145,5 +198,79 @@ class _ImageCard extends StatelessWidget {
         ],
       ),
     );
+    // return Card(
+    //   margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+    //   clipBehavior: Clip.antiAlias,
+    //   child: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.stretch,
+    //     mainAxisSize: MainAxisSize.min,
+    //     mainAxisAlignment: MainAxisAlignment.start,
+    //     children: [
+    //       CachedNetworkImage(
+    //         imageUrl: url,
+    //         height: 200,
+    //         memCacheHeight: 300,
+    //         maxHeightDiskCache: 300,
+    //         width: double.infinity,
+    //         fit: BoxFit.cover,
+    //         errorWidget: (context, url, dynamic error) => Container(
+    //           height: 200,
+    //           padding: const EdgeInsets.symmetric(horizontal: 12.0),
+    //           child: const Center(
+    //             child: Text('Bild konnte nicht geladen werden'),
+    //           ),
+    //         ),
+    //         placeholder: (context, url) => Container(
+    //           height: 200,
+    //           padding: const EdgeInsets.symmetric(horizontal: 12),
+    //           child: Center(
+    //             child: Column(
+    //               mainAxisSize: MainAxisSize.min,
+    //               crossAxisAlignment: CrossAxisAlignment.center,
+    //               mainAxisAlignment: MainAxisAlignment.center,
+    //               children: const [
+    //                 CircularProgressIndicator(),
+    //                 Text('Bild wird geladen'),
+    //               ],
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //       Padding(
+    //         padding: const EdgeInsets.all(8),
+    //         child: Column(
+    //           crossAxisAlignment: CrossAxisAlignment.stretch,
+    //           mainAxisSize: MainAxisSize.min,
+    //           mainAxisAlignment: MainAxisAlignment.start,
+    //           children: const [
+    //             Text(
+    //               'Lorem ipsum dolor sit amet, consetetur sadipscing elitr',
+    //               style: TextStyle(
+    //                 fontSize: 17.5,
+    //                 fontWeight: FontWeight.w600,
+    //               ),
+    //               maxLines: 2,
+    //               textAlign: TextAlign.left,
+    //               overflow: TextOverflow.ellipsis,
+    //             ),
+    //             Text(
+    //               'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, '
+    //               'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, '
+    //               'sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+    //               style: TextStyle(
+    //                 fontSize: 15,
+    //                 height: 1.2,
+    //               ),
+    //               maxLines: 5,
+    //               overflow: TextOverflow.ellipsis,
+    //               textAlign: TextAlign.justify,
+    //               softWrap: true,
+    //             ),
+    //           ],
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
 
 import 'image_page.dart';
 
@@ -6,15 +7,37 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late bool showPerformanceOverlay;
+
+  @override
+  void initState() {
+    super.initState();
+
+    showPerformanceOverlay = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(useMaterial3: true),
-      darkTheme: ThemeData.dark(useMaterial3: true),
-      home: const ImagePage(),
+    return CupertinoApp(
+      home: ImagePage(
+        onToggleShowPerformanceOverlay: () =>
+            setState(() => showPerformanceOverlay = !showPerformanceOverlay),
+      ),
+      showPerformanceOverlay: showPerformanceOverlay,
     );
+
+    // return MaterialApp(
+    //   theme: ThemeData.light(useMaterial3: true),
+    //   darkTheme: ThemeData.dark(useMaterial3: true),
+    //   home: const ImagePage(),
+    // );
   }
 }
